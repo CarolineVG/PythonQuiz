82892d459075328bb24e6205fe872f7c1f17045d
Roel <roelifant@gmail.com>
2019-10-25 15:31:48 +0200

    socket experiments can now handle a complete quiz

--- sockets/client.py
+++ sockets/client.py
@@ -1,90 +1,101 @@
 import socket
 import pickle
 import json
+import sys
 
-HEADERSIZE = 10
+HEADERSIZE = 10 #size of the header of the data we send to the server. In the header we say how long the data is.
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((socket.gethostname(), 1236))
 
-yourScore = 0
-
 clientName = input("who are you? ")
+
+full_msg = b''
+new_msg = True
 while True:
+    msg = s.recv(8)
+    if new_msg:
+        msglen = int(msg[:HEADERSIZE])
+        new_msg = False
 
-    full_msg = b''
-    new_msg = True
-    while True:
-        msg = s.recv(8)
-        if new_msg:
-            msglen = int(msg[:HEADERSIZE])
-            new_msg = False
-
-        full_msg += msg
-        #print(full_msg)
+    full_msg += msg
+    #print(full_msg)
         
-        if len(full_msg)-HEADERSIZE == msglen:
+    if len(full_msg)-HEADERSIZE == msglen:
 
-            d = pickle.loads(full_msg[HEADERSIZE:])
-            message = json.loads(d)
+        d = pickle.loads(full_msg[HEADERSIZE:])
+        message = json.loads(d)
 
-            #print(message)
+        #print(message)
             
-            if message["type"] == "question":
-                print(message['question'])
-                print('- A: '+message['options']['A'])
-                print('- B: '+message['options']['B'])
-                if 'C' in message['options']:
-                    print('- C: '+message['options']['C'])
-                if 'D' in message['options']:
-                    print('- D: '+message['options']['D'])
+        if message["type"] == "question":
+            print("")
+            print(message['question'])
+            print('- A: '+message['options']['A'])
+            print('- B: '+message['options']['B'])
+            if 'C' in message['options']:
+                print('- C: '+message['options']['C'])
+            if 'D' in message['options']:
+                print('- D: '+message['options']['D'])
 
-                # Send answer
-                while True:
-                    answer = input("Choose your option: ")
-                    answer = answer.upper()
-                    if answer == "A" or answer == "B" or answer == "C" or answer == "D":
-                        break
-                    else:
-                        print("Please choose between A, B, C or D")
-                        continue
-                answer = '{"sender":"'+clientName+'", "answer":"'+answer+'"}'
-                answer = pickle.dumps(answer)
-                answer = bytes(f'{len(answer):<{HEADERSIZE}}', "utf-8") + answer
+            # Send answer
+            while True:
+                print("")
+                answer = input("Your answer: ")
+                answer = answer.upper()
+                if answer == "A" or answer == "B" or answer == "C" or answer == "D":
+                    break
+                else:
+                    print("Please choose between A, B, C or D")
+                    continue
+            answer = '{"sender":"'+clientName+'", "answer":"'+answer+'"}'
+            answer = pickle.dumps(answer)
+            answer = bytes(f'{len(answer):<{HEADERSIZE}}', "utf-8") + answer
 
-                s.sendall(answer)
-                print("Waiting for others to answer...")
+            s.sendall(answer)
+            print("")
+            print("Waiting for others to answer...")
 
-            if message["type"] == "scores":
+        if message["type"] == "scores":
 
-                #print(message["scoreboard"])
+            #print(message["scoreboard"])
 
-                # get users score from dictionary and print it
-                yourScore = message["scoreboard"].get(clientName)
-                print(f"Your current score is {yourScore}")
+            # get users score from dictionary and print it
+            yourScore = message["scoreboard"].get(clientName)
+            print("")
+            print(f"Your current score is {yourScore}")
 
-                #dictionary to list so it can be sorted
-                top5 = []
-                for record in message["scoreboard"]:
-                    top5.append([record, message["scoreboard"][record]])
-                top5.sort(key=lambda x: x[1], reverse=True)
+            #dictionary to list so it can be sorted
+            top5 = []
+            for record in message["scoreboard"]:
+                top5.append([record, message["scoreboard"][record]])
+            top5.sort(key=lambda x: x[1], reverse=True)
 
 
-                #print top 5
-                
-                print(f" - 1: {top5[0][1]} - {top5[0][0]}")
-                if len(top5) >= 2:
-                    print(f" - 2: {top5[1][1]} - {top5[1][0]}")
-                if len(top5) >= 3:
-                    print(f" - 3: {top5[2][1]} - {top5[2][0]}")
-                if len(top5) >= 4:
-                    print(f" - 4: {top5[3][1]} - {top5[3][0]}")
-                if len(top5) >= 5:
-                    print(f" - 5: {top5[4][1]} - {top5[4][0]}")
+            #print top 5
+            print("")
+            print("Current scoreboard:")
+            print(f" - 1: {top5[0][1]} - {top5[0][0]}")
+            if len(top5) >= 2:
+                print(f" - 2: {top5[1][1]} - {top5[1][0]}")
+            if len(top5) >= 3:
+                print(f" - 3: {top5[2][1]} - {top5[2][0]}")
+            if len(top5) >= 4:
+                print(f" - 4: {top5[3][1]} - {top5[3][0]}")
+            if len(top5) >= 5:
+                print(f" - 5: {top5[4][1]} - {top5[4][0]}")
+            print("")
+            print("Prepare yourself for the next question...")
+            print("")
 
-            #reset loop
-            new_msg = True
-            full_msg = b''
+        if message["type"] == "end":
+            print("this quiz has ended.")
+            print(message["message"])
+            break
 
-    print(full_msg)
+        #reset loop
+        new_msg = True
+        full_msg = b''
+print("exiting program")
+sys.exit(0)
     
--- sockets/server.py
+++ sockets/server.py
@@ -3,14 +3,83 @@ import pickle
 import random
 import json
 import threading
+import time
 
-HEADERSIZE = 10
-clients = set()
-answers = 0
-scores = {}
+HEADERSIZE = 10 #size of the header of the data we send to the client. In the header we say how long the data is.
+clients = set() #a set of all clients that are connected
+answers = 0 #a counter that keeps track of how many clients have answered already
+scores = {} #the scoreboard
 
-def sendQuestion(clientsocket, address, question, solution):
-    print("sending question...")
+questions = [ #list of (for now hard-coded) questions that the clients will answer
+        {
+            'id': '0001',
+            'question': 'What is the first letter of the alphabet?',
+            'options':{
+                'A':'A',
+                'B':'B',
+                'C':'C',
+                'D':'D'
+            },
+            'solution':'A'
+        },
+        {
+            'id': '0002',
+            'question': 'What colour is the sky?',
+            'options':{
+                'A':'Green',
+                'B':'Red',
+                'C':'Blue',
+                'D':'brown'
+            },
+            'solution':'C'
+        },
+        {
+            'id': '0003',
+            'question': 'Who is the best python programmer?',
+            'options':{
+                'A':'Roel',
+                'B':'Caroline',
+                'C':'Santa Claus'
+            },
+            'solution':'B'
+        },
+        {
+            'id': '0004',
+            'question': 'What is the airspeed velocity of an unladen swallow?',
+            'options':{
+                'A':'I don\'t know that.',
+                'B':'Blue!',
+                'C':'That depends. Is it an African swallow or a European one?'
+            },
+            'solution':'C'
+        },
+        {
+            'id': '0005',
+            'question': 'Was this a fun quiz?',
+            'options':{
+                'A':'Yes!',
+                'B':'No.'
+            },
+            'solution':'B'
+        }
+    ]
+
+currentQuestion = 0 # position of the next question to be send
+
+def sendToAll(json):
+    json = pickle.dumps(json)
+    json = bytes(f'{len(json):<{HEADERSIZE}}', "utf-8") + json
+    for c in clients:
+        c.sendall(json)
+        #print(f"sending to {c}")
+
+def sendQuestion(clientsocket, address, question):
+
+    solution = question['solution']
+    question = '{"type":"question", "sender": "Host", "id":"'+question['id']+'", "question": "'+question['question']+'", "options":'+json.dumps(question['options'])+'}'
+
+    #print(question)
+    #print(solution)
     
     #send question
     msg = pickle.dumps(question)
@@ -20,6 +89,7 @@ def sendQuestion(clientsocket, address, question, solution):
     #print(msg)
     
     clientsocket.send(msg)
+    print(f"send a new question to {address}")
 
     #await response
     full_answer = b''
@@ -49,20 +119,37 @@ def sendQuestion(clientsocket, address, question, solution):
                     scores[answer["sender"]] = 1
             elif answer["sender"] not in scores:
                 scores[answer["sender"]] = 0
-
             if answers == len(clients):
                 scoreboard = '{"type":"scores", "scoreboard":'+json.dumps(scores)+'}'
-                scoreboard = pickle.dumps(scoreboard)
-                scoreboard = bytes(f'{len(scoreboard):<{HEADERSIZE}}', "utf-8") + scoreboard
-                #print(scoreboard)
-                for c in clients:
-                    c.sendall(scoreboard)
+                sendToAll(scoreboard)
+                print("scores have been send")
+                answers = 0
+                global currentQuestion
+                currentQuestion = currentQuestion + 1
+            return
 
-def connect():
+def handleQuiz(clientsocket, address, questions):
+    position = currentQuestion
+    sendQuestion(clientsocket, address, questions[0])
+    while True:
+        if position < currentQuestion:
+            position = position + 1
+            time.sleep(1)
+            if position != len(questions):
+                sendQuestion(clientsocket, address, questions[position])
+            else:
+                break
+    endMsg = pickle.dumps('{"type":"end", "message":"Thank you for playing!"}') #this message could be a custom variable
+    endMsg = bytes(f'{len(endMsg):<{HEADERSIZE}}', "utf-8") + endMsg
+    clientsocket.send(endMsg)
+    return
+
+def startQuiz():
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.bind((socket.gethostname(), 1236))
     s.listen(5)
     while True:
+        print("listening for new players")
         #connect
         clientsocket, address = s.accept()
         print(f"Connection from {address} has been established!")
@@ -70,8 +157,7 @@ def connect():
         global clients
         clients.add(clientsocket)
 
-        x = threading.Thread(target=sendQuestion, args=(clientsocket, address, '{"type":"question", "sender": "Host", "id": "0001", "question": "What is the first letter of the alphabet?", "options":{"A":"A","B":"B","C":"C"}}', 'A'))
+        x = threading.Thread(target=handleQuiz, args=(clientsocket, address, questions))
         x.start()
 
-
-connect()
+startQuiz()
